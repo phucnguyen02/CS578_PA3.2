ba3e0f9aff739d06bc5bc2c395feb3de2f73dcb5
phucnguyen02 <ndhp02@gmail.com>
2023-12-01 11:11:25 -0500

    Design doc

--- gigapaxos/README.md
+++
@@ -1,4 +0,0 @@
-# gigapaxos
-
-Refer to the Getting Started page in the
-[Wiki](<https://github.com/MobilityFirst/gigapaxos/wiki>) above for tutorials.
--- gigapaxos/src/edu/umass/cs/chainreplication/chainutil/ReplicatedChainException.java
+++
@@ -1,9 +0,0 @@
-package edu.umass.cs.chainreplication.chainutil;
-
-public class ReplicatedChainException extends RuntimeException{
-
-    public ReplicatedChainException(String msg) {
-        super(msg);
-    }
-
-}
